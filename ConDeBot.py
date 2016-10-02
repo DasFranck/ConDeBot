3034bfb19e08c753f38da3443fbb00909b3eefc6
DasFranck <dasfranck@gmx.com>
2016-10-02 22:38:38 +0200

    Replier module done (with count function(

--- ConDeBot.py
+++ ConDeBot.py
@@ -112,13 +112,12 @@ def on_message(message):
             yield from opmod.main(client, logger, message, action, args, nick)
 
         # Display the commands call count (Module: "replier")
-#        elif (action in ["count"]):
-#            yield from replier.count(client, logger, message, action, nick)
+        elif (action in ["count"]):
+            yield from replier.count(client, logger, message, action, args, nick)
 
         # If it's not a built-in command, check if it's related to replies
         else:
-            if (not (yield from replier.main(client, logger, message, action, args, nick))):
-                yield from client.send_message(chan, "I know nothing about this. I swear!")
+            yield from replier.main(client, logger, message, action, args, nick)
     return
 
 
--- modules/replier.py
+++ modules/replier.py
@@ -5,23 +5,90 @@ REPLIES_FILE_PATH = "jsonfiles/"
 REPLIES_FILE = REPLIES_FILE_PATH + "replies.json"
 
 try:
+    from collections import OrderedDict
     import os
-    import json
+    import hjson
 except ImportError as message:
     print("Missing package(s) for the replier module: %s" % message)
     exit(12)
 
-# return False if the command asked doesn't exist, return True otherwise or when setting
-async def main(client, logger, message, action, args, nick):
-    #if (os.path.isfile(REPLIES_FILE)):
-    #    with open(REPLIES_FILE) as replies_file:
-    #        replies = json.load(replies_file)
-    #else:
-    #    return False
-    if (args is None or len(args) == 0):
+# TODO : ADD LOGGING FUNCTION CALL
 
-    elif (args[1] == "="):
 
+# Get the reply dict assign to the trigger
+def get_reply(replies, trigger):
+    if (replies is None):
+        return None
+    for reply in replies:
+        if (reply["trigger"] == trigger):
+            return reply
+    return None
+
+
+# Load the replies file into an array of dict
+async def load_replies(client, message):
+    if (os.path.isfile(REPLIES_FILE)):
+        with open(REPLIES_FILE) as replies_file:
+            try:
+                return hjson.load(replies_file)
+            except:
+                await client.send_message(message.channel, "The JSON replies file seems corrupted. Please fix it before using the replier module.")
+                return None
     else:
+        return []
 
-    return False
+
+# Print the number of times a message has been triggered
+async def count(client, logger, message, action, args, nick):
+    replies = await load_replies(client, message)
+    if (replies is None):
+        return
+    if (args is None or len(args) == 0):
+        await client.send_message(message.channel, "Try with an argument for this command next time.")
+        return
+    else:
+        for arg in args:
+            reply = get_reply(replies, arg)
+            if (reply is None):
+                await client.send_message(message.channel, "The trigger %s doesn't even exist." % arg)
+            else:
+                await client.send_message(message.channel, "The trigger %s has been called %d times." % (arg, reply["count"]))
+
+
+# Manage the printing and setting of triggered messages
+async def main(client, logger, message, action, args, nick):
+    replies = await load_replies(client, message)
+    if (replies is None):
+        return
+    # Call the triggered message
+    if (args is None or len(args) == 0):
+        reply = get_reply(replies, action)
+        if (reply is not None):
+            await client.send_message(message.channel, reply["message"])
+            reply["count"] += 1
+            with open(REPLIES_FILE, 'w') as replies_file:
+                hjson.dump(replies, replies_file, indent=' ' * 2)
+        else:
+            await client.send_message(message.channel, "I know nothing about this. I swear!")
+
+    # Assign a message to this trigger
+    elif (args[0] == "="):
+        if (len(args) > 1):
+            # Check if the reply dict already exist
+            old_dict = get_reply(replies, action)
+            # If the reply dict exist, replace it
+            if (old_dict is None):
+                new_dict = OrderedDict(trigger=action,
+                                       message=" ".join(args[1:]),
+                                       count=0)
+                replies.append(new_dict)
+            else:
+                old_dict["trigger"] = action
+                old_dict["message"] = " ".join(args[1:])
+                old_dict["count"] = 0
+            await client.send_message(message.channel, "Roger that, %s trigger has been registered." % action)
+            with open(REPLIES_FILE, 'w') as replies_file:
+                hjson.dump(replies, replies_file, indent=' ' * 2)
+        else:
+            await client.send_message(message.channel, "You should try to put a message to assign to this trigger.")
+    return
