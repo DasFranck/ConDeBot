05577f001d3db7e4e86090d813ea32dce22de716
DasFranck Hochstaetter <dasfranck@gmx.com>
2016-12-08 14:45:18 +0100

    Coffee module now serves tea and use a json files of quotes instead of hardcoded quotes

--- modules/coffee.py
+++ modules/coffee.py
@@ -2,22 +2,32 @@
 # -*- coding: utf-8 -*-
 
 NAME = "ConDeBot"
-QUOTES = ["Without sugar.", "With one sugar.", "With too much sugar.",
-          "With a pinch of sugar.", "With three spoon of salt because I hate you so much.",
-          "With a bit of milk", "Wait did I put a sugar? I'm not sure...",
-          "Decaffeinated, cause it's way too strong for a little weak buddy like you.", ""]
 
 try:
     import random
+    import json
 except ImportError as message:
     print("Missing package(s) for %s: %s" % (NAME, message))
     exit(12)
 
 
-def quote(author, args):
+def coffee(author, args):
     # Check if the coffee is for someone else (And if the sender didn't forget the recipient)
-    if ('>' in args):
-        index = args.index('>') + 1
-        return ("Here " + " ".join(args[index:]) + ", that's your coffee.\n" + random.choice(QUOTES))
-    else:
-        return ("Here " + author + ", that's your coffee.\n" + random.choice(QUOTES))
+    with open("jsonfiles/coffee.json", 'r') as quotes_file:
+        quotes = json.load(quotes_file)
+        if ('>' in args):
+            index = args.index('>') + 1
+            return ("Here " + " ".join(args[index:]) + ", that's your coffee.\n" + random.choice(quotes["coffee"]))
+        else:
+            return ("Here " + author + ", that's your coffee.\n" + random.choice(quotes["coffee"]))
+
+
+def tea(author, args):
+    # Check if the coffee is for someone else (And if the sender didn't forget the recipient)
+    with open("jsonfiles/coffee.json", 'r') as quotes_file:
+        quotes = json.load(quotes_file)
+        if ('>' in args):
+            index = args.index('>') + 1
+            return ("Here " + " ".join(args[index:]) + ", that's your tea.\n" + random.choice(quotes["tea"]))
+        else:
+            return ("Here " + author + ", that's your tea.\n" + random.choice(quotes["tea"]))
