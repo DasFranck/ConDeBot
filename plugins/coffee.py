ea7b98460e238b634be470c19bb92aed429f3d7e
DasFranck (DASPAT) <dasfranck@gmx.com>
2017-06-06 14:33:06 +0200

    Linting plugins/coffee.py

--- plugins/coffee.py
+++ plugins/coffee.py
@@ -16,21 +16,36 @@ class CoffeePlugin(Plugin):
 
         # Serve a delicious coffee (Module: "coffee")
         if action in ["café", "cafe", "coffee"]:
-            self.cdb.logger.log_info_command("Coffee requested by " + author, message)
+            self.cdb.logger.log_info_command("Coffee requested by " + author,
+                                             message)
             await self.cdb.send_message(message.channel, ":coffee:")
-            await self.cdb.send_message(message.channel, self.serve(message, args, "coffee"))
+            await self.cdb.send_message(message.channel,
+                                        self.serve(message, args, "coffee"))
         # Serve a delicious tea (Module: "coffee")
         elif action in ["thé", "the", "tea"]:
-            self.cdb.logger.log_info_command("Tea requested by " + author, message)
+            self.cdb.logger.log_info_command("Tea requested by " + author,
+                                             message)
             await self.cdb.send_message(message.channel, ":tea:")
-            await self.cdb.send_message(message.channel, self.serve(message, args, "tea"))
+            await self.cdb.send_message(message.channel,
+                                        self.serve(message, args, "tea"))
 
     def serve(self, message, args, drink):
-        # Check if the coffee is for someone else (And if the sender didn't forget the recipient)
+        """
+        Check if the coffee is for someone else
+        (And if the sender didn't forget the recipient)
+        """
         with open("jsonfiles/coffee.json", 'r') as quotes_file:
             quotes = json.load(quotes_file)
             if ('>' in args):
                 index = args.index('>') + 1
-                return ("Here {}, that's your {}.\n{}".format(" ".join(args[index:]), drink, random.choice(quotes[drink])))
+                return ("Here {}, that's your {}.\n{}".format(
+                    " ".join(args[index:]),
+                    drink,
+                    random.choice(quotes[drink]))
+                )
             else:
-                return ("Here {}, that's your {}.\n{}".format(message.author.mention, drink, random.choice(quotes[drink])))
+                return ("Here {}, that's your {}.\n{}".format(
+                    message.author.mention,
+                    drink,
+                    random.choice(quotes[drink]))
+                )
