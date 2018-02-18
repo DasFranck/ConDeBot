5809f7cbcc23ebbd27a289b47c5807d38e612459
DasFranck (DasPV) <dasfranck@gmx.com>
2018-02-18 23:18:40 +0000

    Docker update (And it's working)

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM python:alpine3.7
 
-WORKDIR /usr/src/app
+RUN mkdir -p /app/ConDeBot
+RUN mkdir -p /data/ConDeBot/
+WORKDIR /app/ConDeBot
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+COPY ./docker/config ./config
+RUN rm -r ./docker/
 
 ENTRYPOINT ["python", "./ConDeBot.py" ]
--- docker/config/config.py
+++ docker/config/config.py
@@ -6,8 +6,7 @@ SHORT_NAME = "CDB"
 DESCRIPTION = "ConDeBot - Un con de bot Discord"
 CMD_PREFIX = ""
 
-# Those path concatenations looks a bit insecure
-DATA_PATH = "./data/"
+DATA_PATH = "/data/ConDeBot/"
 OPS_FILE_PATH = DATA_PATH + "data/jsonfiles/"
 OPS_FILE = DATA_PATH + OPS_FILE_PATH + "ops.json"
 REPLIES_FILE_DIR = DATA_PATH + "jsonfiles/replies/"
