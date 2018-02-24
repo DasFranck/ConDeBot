bb2f7a1d4ea2495e389a915428bb249bb7ad860e
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-02-24 01:30:13 +0100

    Fix travis.yml

--- travis/trigger_docker_build.sh
+++ travis/trigger_docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ "$BRANCH" = "dev" ] || [ "$BRANCH" = "master" ]; then 
-    curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$BRANCH"}' -X POST https://registry.hub.docker.com/u/dasfranck/condebot/trigger/${DOCKER_TRIGGER_TOKEN}/
-fi
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Building docker images for the $BRANCH branch"
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$BRANCH\"}" -X POST https://registry.hub.docker.com/u/dasfranck/condebot/trigger/${DOCKER_TRIGGER_TOKEN}/
