1058620750a3ceb26dfdafbd5a5d9870ca8a31f9
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-02-25 13:09:28 +0100

    Updating trigger_docker_build.sh with Travis env variables.

--- travis/trigger_docker_build.sh
+++ travis/trigger_docker_build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "Building docker images for the $BRANCH branch"
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$BRANCH\"}" -X POST https://registry.hub.docker.com/u/dasfranck/condebot/trigger/${DOCKER_TRIGGER_TOKEN}/
+echo "Triggering a docker image build for the $TRAVIS_BRANCH branch"
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST https://registry.hub.docker.com/u/dasfranck/condebot/trigger/${DOCKER_TRIGGER_TOKEN}/
