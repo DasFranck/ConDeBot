06f84281990c2584849631c5fc27c707cdd3f1cf
DasFranck <dasfranck@gmx.com>
2021-01-18 19:16:26 +0100

    Comment out unused Plugin functions for compatibility purpose

--- classes/Plugin.py
+++ classes/Plugin.py
@@ -12,65 +12,65 @@ class Plugin:
     async def on_message(self, message):
         pass
 
-    async def on_message_edit(self, before, after):
-        pass
+    # async def on_message_edit(self, before, after):
+    #     pass
 
-    async def on_message_delete(self, message):
-        pass
+    # async def on_message_delete(self, message):
+    #     pass
 
-    async def on_reaction_add(self, reaction, user):
-        pass
+    # async def on_reaction_add(self, reaction, user):
+    #     pass
 
-    async def on_reaction_remove(self, reaction, user):
-        pass
+    # async def on_reaction_remove(self, reaction, user):
+    #     pass
 
-    async def on_reaction_clear(self, message, reactions):
-        pass
+    # async def on_reaction_clear(self, message, reactions):
+    #     pass
 
-    async def on_channel_create(self, channel):
-        pass
+    # async def on_channel_create(self, channel):
+    #     pass
 
-    async def on_channel_update(self, before, after):
-        pass
+    # async def on_channel_update(self, before, after):
+    #     pass
 
-    async def on_channel_delete(self, channel):
-        pass
+    # async def on_channel_delete(self, channel):
+    #     pass
 
-    async def on_member_join(self, member):
-        pass
+    # async def on_member_join(self, member):
+    #     pass
 
-    async def on_member_remove(self, member):
-        pass
+    # async def on_member_remove(self, member):
+    #     pass
 
-    async def on_member_update(self, before, after):
-        pass
+    # async def on_member_update(self, before, after):
+    #     pass
 
-    async def on_guild_join(self, guild):
-        pass
+    # async def on_guild_join(self, guild):
+    #     pass
 
-    async def on_guild_remove(self, guild):
-        pass
+    # async def on_guild_remove(self, guild):
+    #     pass
 
-    async def on_guild_update(self, before, after):
-        pass
+    # async def on_guild_update(self, before, after):
+    #     pass
 
-    async def on_guild_role_create(self, guild, role):
-        pass
+    # async def on_guild_role_create(self, guild, role):
+    #     pass
 
-    async def on_guild_role_delete(self, guild, role):
-        pass
+    # async def on_guild_role_delete(self, guild, role):
+    #     pass
 
-    async def on_guild_role_update(self, guild, role):
-        pass
+    # async def on_guild_role_update(self, guild, role):
+    #     pass
 
-    async def on_voice_state_update(self, before, after):
-        pass
+    # async def on_voice_state_update(self, before, after):
+    #     pass
 
-    async def on_member_ban(self, member):
-        pass
+    # async def on_member_ban(self, member):
+    #     pass
 
-    async def on_member_unban(self, member):
-        pass
+    # async def on_member_unban(self, member):
+    #     pass
 
-    async def on_typing(self, channel, user, when):
-        pass
+    # async def on_typing(self, channel, user, when):
+    #     pass
